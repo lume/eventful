71ed767cc9a7bee3645cbcee9346ca5784f654e5
Joe Pea <joe@trusktr.io>
2023-10-24 01:12:57 -0700

    v0.3.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 export * from './Eventful.js';
-export declare const version = "0.2.5";
+export declare const version = "0.3.0";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
 export * from './Eventful.js';
-export const version = '0.2.5';
+export const version = '0.3.0';
 //# sourceMappingURL=index.js.map
