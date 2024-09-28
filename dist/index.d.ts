4b3cc8ba97449e8cf4993d7e3872ab87745063ed
Joe Pea <joe@trusktr.io>
2024-09-27 19:30:17 -0700

    remove the no longer working emits decorator

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
 export * from './Eventful.js';
-export * from './emits.js';
 export declare const version = "0.3.2";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
 export * from './Eventful.js';
-export * from './emits.js';
 export const version = '0.3.2';
 //# sourceMappingURL=index.js.map
